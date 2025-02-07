f57f4dbcd498df1bb9050cdf888e4edacbc7437a
Aminat <aminatdahunsi08@gmail.com>
2025-02-07 10:51:03 -0600

    done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 // Components
 import LandingPage from './Pages/Landing';
-
+import Team from './Pages/Team.jsx'
 function App() {
   return (
     <div>
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/team" element={<Team/>}/>
         </Routes>
       </Router>
     </div>
